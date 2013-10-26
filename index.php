9f0d02a6744fb6163d6ada39bdd6e6fa77c6c9f1
sureshprasanna70 <sureshprasanna70@gmail.com>
2013-10-26 21:32:26 +0530

    added session

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
+<?session_start();?>
 <html>
 <head>
-<link href='http://fonts.googleapis.com/css?family=Sorts+Mill+Goudy' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
-<script src="js/jquery.js"></script>
-<script src="js/custom.js"></script>
-
 <title>
   CUIC|HOME
 </title>
@@ -15,7 +11,30 @@
 
 <body>
 <?php include('menu.php');?>
-
+<div class="container">
+	<div class="row-fluid">
+		<div class="span12">
+			<form name="search" action="search.php" method="post">
+				
+					<div class="span2">
+						<input type="text" name="term" size="200">
+					</div>
+					<div class="span4 offset2">
+						<input type="radio" name="cla" value="author">PROF
+						<input type="radio" name="cla" value="area">SUB
+						<input type="radio" name="cla" value="name">TITLE
+					</div>
+					<br>
+					<div class="span12 form-controls">
+						<div class="controls">
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" class="btn btn-success" value="Get it">
+						</div>
+					</div>
+					</div>
+					</form>
+					</div>
+					</div>
+					</div>					
 	
 </body>
 
