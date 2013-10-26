40cc4d2b23fc5951075ddc347d6a40a1d70408bf
sureshprasanna70 <sureshprasanna70@gmail.com>
2013-10-26 21:41:21 +0530

    dashboard

---
+++ adminpanel.php
@@ -0,0 +1,17 @@
+<?session_start();?>
+<html>
+<head>
+<link href='http://fonts.googleapis.com/css?family=Sorts+Mill+Goudy' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<title>
+	CLIB|PANEL
+</title>
+<body>
+	<?include('menu.php');?>
+	<a href="addpage.php">Add a new page</a><br><br><br>
+	<a href="addbook.php">Add book to collection </a>
+</body>
+</html>
